87b8f7b9da61e55c6ae01ba62fa6854293ff1bcd
KRO5RT <robert.kohn@bosch.com>
2022-09-01 16:16:05 +0200

    initial commit

---
+++ src/test/java/com/example/HelloEurekaService/HelloEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloEurekaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
